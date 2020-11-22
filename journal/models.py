36e16d69a9b76b7780d76fd62b4ff0f8756c2ab3
Shankar Nag Nemmani <shankar.nemmani@yahoo.com>
2020-11-22 09:57:50 +0530

    Data Modeling Version 1

--- journal/models.py
+++ journal/models.py
@@ -2,6 +2,33 @@ from django.db import models
 from django.db.models import CASCADE
 
 # Create your models here.
+class TaskState(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+
+class Task(models.Model):
+    """A task/to-do item in Bullet Journal"""
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=250)
+    task_state = models.ForeignKey("TaskState", related_name="task_state", on_delete=CASCADE)
+    due_date = models.DateTimeField(null=True)
+    recurrance = models.IntegerField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    future_log = models.BooleanField(default=False)
+
+
+class Event(models.Model):
+    """An Event in Bullet Journal"""
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=250)
+    due_date = models.DateTimeField(null=True)
+    recurrance = models.IntegerField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
 class JournalEntryType(models.Model):
     """Entry types are basic journal item types like Task/Event/Note"""
     id = models.AutoField(primary_key=True)
@@ -36,3 +63,12 @@ class Index(models.Model):
     page_number = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+class FutureLog(models.Model):
+    id = models.AutoField(primary_key=True)
+    entry = models.ForeignKey("JournalEntry", related_name="journal_entry_fl", on_delete=CASCADE)
+    month = models.IntegerField(choices=[1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11, 12])
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 # Create your views here.
+class EntryListView(APIView):
+    """
+    List all entries, create a new entry
+    """
+    pass
