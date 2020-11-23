49f71a6f2f5a145cdc663a67895a0fda872c3c3f
Shankar Nag Nemmani <shankar.nemmani@yahoo.com>
2020-11-23 21:31:15 +0530

    Heroku push

--- bullet_journal/settings.py
+++ bullet_journal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$^3fqh5u-ne*!5a*8hbn!d=kl5m_yk0%5yo^uust-v=u0n09m='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
