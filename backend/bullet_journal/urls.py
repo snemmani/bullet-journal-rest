56de203f7e913962a031d6c2f4f7d10b10749279
snemmani <shankar.nemmani@yahoo.com>
2022-02-03 19:34:18 +0530

    Complete close #10

--- backend/bullet_journal/urls.py
+++ backend/bullet_journal/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/journal/', include('journal.urls')),
     path('reference/', include('swagger.urls')),
+    url(r'^.*', TemplateView.as_view(template_name="home.html"), name="home")
 ]
